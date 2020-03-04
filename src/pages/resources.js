db551f5609a562f5f505475b123acd5b2b4c3242
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2020-03-03 21:20:47 -0500

    update

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "../res/styles/resources.css";
 
 const Resources = () => {
     return (
         <div>
-            <h1>Resources Page</h1>
+            <h1 className="test">Resources Page</h1>
         </div>
     )
 }
